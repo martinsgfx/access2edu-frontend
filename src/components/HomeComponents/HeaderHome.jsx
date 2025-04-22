b88c6f7eb844f7c3dc28994edfd1d2932cac4629
Martinsgfx <martos.stifler@gmail.com>
2025-04-22 19:25:11 +0100

    Connected the Login and Signup Buttons on the Home page to the Login and Signup page

--- src/components/HomeComponents/HeaderHome.jsx
+++ src/components/HomeComponents/HeaderHome.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import "../../styles/global.css";
 
 const HeaderHome = () => {
   return (
     <header className="header">
-      <div className="logo"> <img src="src\assets\access_II_edu-logo.png" alt="" /></div>
+      <div className="logo">
+        <img src="src\assets\access_II_edu-logo.png" alt="" />
+      </div>
       <nav>
         <ul className="nav-links">
           <li>Home</li>
           <li>Streams</li>
           <li>Admission</li>
           <li>About</li>
-          <li>Log in</li>
-          <li><button className="signup-button">Sign Up</button></li>
+          <li>
+            <Link to="/login" className="login-link">Log in</Link>
+          </li>
+          <li>
+            <Link to="/signup">
+              <button className="signup-button">Sign Up</button>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
